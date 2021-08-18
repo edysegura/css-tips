c424a6d88779803161effcb6537d51fda5332fa0
Edy Segura <ednardo.segura@mundiale.com.br>
2021-08-17 22:18:39 -0300

    chore: change indentation to use 2 spaces

--- transition/index.js
+++ transition/index.js
@@ -3,9 +3,9 @@
 const node = document.querySelector('.hello')
 
 node.addEventListener('click', (event) => {
-    event.target.classList.add('hello-active')
+  event.target.classList.add('hello-active')
 })
 
 node.addEventListener('transitionend', (event) => {
-    event.target.classList.remove('hello-active')
+  event.target.classList.remove('hello-active')
 })
